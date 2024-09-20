5337ca6b1651e3aa8887fc8d8316e16ce40b4230
Sukma Bagus Wahasdwika <sukmabaguswahasdwika10@gmail.com>
2024-09-20 09:44:51 +0700

    Praktikum 1

--- pertemuan3/praktikum1.dart
+++ pertemuan3/praktikum1.dart
@@ -1,4 +1,12 @@
 void main() {
+  /*String test = "test2";
+  if (test == "test1") {
+    print("Test1");
+  } else if (test === "test2") {
+    print("Test2");
+  } else {
+    print("Something else")
+  }*/
   String test = "true";
   if (test=="true") {
     print("Kebenaran");
