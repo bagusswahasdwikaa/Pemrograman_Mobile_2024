60321739e34375fc5514f7a31c2ee1a4ab1269b2
Sukma Bagus Wahasdwika <sukmabaguswahasdwika10@gmail.com>
2024-09-26 23:32:46 +0700

    Praktikum-1 dan 2

---
+++ pertemuan4/praktikum1.dart
@@ -0,0 +1,18 @@
+void main() {
+  /*var list = [1, 2, 3];
+  assert(list.length == 3);
+  assert(list[1] == 2);
+  print(list.length);
+  print(list[1]);
+
+  list[1] = 1;
+  assert(list[1] == 1);
+  print(list[1]);*/
+
+  var index = [];
+  index.length = 5;
+  index[1] = "Sukma Bagus Wahasdwika";
+  index[2] = "2241720223";
+  print(index[1]);
+  print(index[2]);
+}
